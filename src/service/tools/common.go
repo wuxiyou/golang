45b8732ca2aacef78e706876228dfc88473cc595
wuxiyou <839744066@qq.com>
2018-01-30 23:04:02 +0800

    模拟POST请求

--- src/service/tools/common.go
+++ src/service/tools/common.go
@@ -8,6 +8,10 @@ import (
 	"encoding/base64"
 	"os"
 	"time"
+	"net/http"
+	"strings"
+	"io/ioutil"
+	"fmt"
 )
 func Md5(s string) string  {
 	md5Htx := md5.New()
@@ -40,5 +44,58 @@ func checkFileIsExist(filename string) bool  {
 func GetCurrentTime() string {
 	t := time.Now().Format("2006-01-02 15:04:05")
 	return t
+}
+
+func HttpPost(url , data string) (string, error){
+	str := strings.NewReader(data)
+	//fmt.Println(str)
+	postReq, err := http.NewRequest("POST", url, str)
+	if err != nil {
+		//fmt.Println(err)
+		return "", err
+	}
+	postReq.Header.Set("Content-Type", "application/json;encoding=utf-8")
+
+	client := &http.Client{}
+	resp, err := client.Do(postReq)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		//fmt.Println(err)
+		return "", err
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			//fmt.Println(err)
+			return "", err
+		}
+		//fmt.Println("POST请求:创建成功", string(body))
+		//fmt.Println(err)
+		return string(body), nil
+	}
+}
+
+func HttpDo() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://www.baidu.com", strings.NewReader("name=cjb"))
+	if err != nil {
+		// handle error
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
 }
 
